ca983e818ac1a7c22cf093009ecdeae094fd99b4
allacee <ddimaglushkov@ya.ru>
2021-05-05 07:29:06 +0300

    #7 Fix issue with DM not starting properly

--- install.sh
+++ install.sh
@@ -55,10 +55,10 @@ function configure_display_manager {
 	cd $repositories
 	git clone https://github.com/allacee/lightdm-webkit2-theme-minimal.git
 	sudo cp -r lightdm-webkit2-theme-minimal/ /usr/share/lightdm-webkit/themes/minimal
-	sudo awk -i inplace -v FS="=" '{if($1 ~/#greeter-session/){print $1, "= lightdm-webkit2-greeter" } else {print $0}}' /etc/lightdm/lightdm.conf
+	sudo awk -i inplace -v FS="=" '{if($1 ~/#greeter-session/){"greeter-session = lightdm-webkit2-greeter" } else {print $0}}' /etc/lightdm/lightdm.conf
 	sudo awk -i inplace -v FS="=" '{if($1 ~/webkit_theme/){print $1, "= minimal" } else {print $0}}' /etc/lightdm/lightdm-webkit2-greeter.conf
-	systemctl enable lightdm --force 
-	
+	systemctl enable lightdm --force
+
 	cd $dotfiles
 }
 
@@ -74,7 +74,7 @@ function install_dependencies {
 	else
 		echo ">> Successfully found yay"
 	fi
-	
+
 	echo "> Installing packages listed at dependencies.txt using"
 	yay -S --needed $(awk -v FS="#" '{print $1}' $dotfiles/$dependencies)
 }
