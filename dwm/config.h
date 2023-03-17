7372357c7378524dc2dc25723749597dfafcb90d
dimaglushkov <ddimaglushkov@ya.ru>
2023-03-17 19:56:14 +0400

    Add tmux config

--- dwm/config.h
+++ dwm/config.h
@@ -108,7 +108,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char terminal[] = "alacritty";
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { terminal, NULL };
+static const char *termcmd[]  = { terminal, "-e", "tmux", NULL };
 static const char scratchpadname[] = "scratchpad";
 
 static const char *scratchpadcmd[] = { terminal, "-t", scratchpadname, "-o", "window.dimensions.columns=120", "-o", "window.dimensions.lines=34", NULL };
